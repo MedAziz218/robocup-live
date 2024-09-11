bd9985966989f63ba44e6405098f888461216cfc
Mohamed Aziz Lahdheri <118969900+MedAziz218@users.noreply.github.com>
2024-09-11 19:32:41 +0200

    added shadcn button added home page shadcn test

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -73,7 +73,10 @@ declare module '$env/static/private' {
 	export const npm_lifecycle_event: string;
 	export const npm_lifecycle_script: string;
 	export const npm_node_execpath: string;
+	export const npm_package_dependencies_bits_ui: string;
 	export const npm_package_dependencies_clsx: string;
+	export const npm_package_dependencies_lucide_svelte: string;
+	export const npm_package_dependencies_mode_watcher: string;
 	export const npm_package_dependencies_tailwind_merge: string;
 	export const npm_package_dependencies_tailwind_variants: string;
 	export const npm_package_description: string;
@@ -225,7 +228,10 @@ declare module '$env/dynamic/private' {
 		npm_lifecycle_event: string;
 		npm_lifecycle_script: string;
 		npm_node_execpath: string;
+		npm_package_dependencies_bits_ui: string;
 		npm_package_dependencies_clsx: string;
+		npm_package_dependencies_lucide_svelte: string;
+		npm_package_dependencies_mode_watcher: string;
 		npm_package_dependencies_tailwind_merge: string;
 		npm_package_dependencies_tailwind_variants: string;
 		npm_package_description: string;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import LoaderCircle from 'lucide-svelte/icons/loader-circle';
+	import { ModeWatcher } from 'mode-watcher';
+	import { LightSwitch } from '$lib/components/custom';
+	import { Button } from '$lib/components/ui/button';
+	import { Separator } from '$lib/components/ui/separator';
+
+	let loading = false;
+	const toggleLoading = () => (loading = !loading);
+	const loadStub = () => {
+		toggleLoading();
+		setTimeout(toggleLoading, 1000);
+	};
+</script>
+
+<ModeWatcher />
+<!-- <Toaster richColors /> -->
+
+<div class="flex h-full flex-col md:flex">
+	<div class="flex flex-row items-center justify-between space-y-2 px-4 py-2 md:h-16">
+		<h2 class="mr-2 mt-2 text-lg font-semibold">Playground</h2>
+		<div class="ml-auto flex space-x-2 sm:justify-end">
+			<Button disabled={loading} on:click={loadStub}>
+				{#if loading}
+					<LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+				{/if}
+				Click Me
+			</Button>
+			<LightSwitch />
+		</div>
+	</div>
+	<Separator />
+</div>
