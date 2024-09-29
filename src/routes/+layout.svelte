c956dabcb84a1b0db3108c36bb209986608e9669
Mohamed Aziz Lahdheri <118969900+MedAziz218@users.noreply.github.com>
2024-09-29 18:57:44 +0200

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script>
 	import '../app.css';
     import { ModeWatcher } from 'mode-watcher';
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
 </script>
 <ModeWatcher defaultMode={"dark"} />
 
